54a1a72d9aaef4d5bea587843ba3592fff4f22eb
Guilherme Graebim <guigraebim3@gmail.com>
2023-05-15 16:26:49 -0300

    ]

---
+++ src/main/java/com/mibeargui/gamelist/controllers/GameListController.java
@@ -0,0 +1,25 @@
+package com.mibeargui.gamelist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mibeargui.gamelist.dto.GameListDTO;
+import com.mibeargui.gamelist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+	@Autowired
+	private GameListService gameListService;
+
+	@GetMapping
+	public List<GameListDTO> findAll(){
+		List<GameListDTO> result = gameListService.findAll();
+		return result;
+	}
+}
---
+++ src/main/java/com/mibeargui/gamelist/services/GameListService.java
@@ -0,0 +1,25 @@
+package com.mibeargui.gamelist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mibeargui.gamelist.dto.GameListDTO;
+import com.mibeargui.gamelist.entities.GameList;
+import com.mibeargui.gamelist.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll(){
+		List<GameList> result = gameListRepository.findAll();
+		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
+		return dto;
+	}
+}
